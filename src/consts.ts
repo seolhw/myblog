13e349ec13ed8ef6b046557d941b462b65f7cb5c
lihuiwang <seolhw@qq.com>
2026-06-29 15:31:04 +0800

    1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-export const SITE_TITLE = 'huiwang 的博客';
-export const SITE_DESCRIPTION = '记录软件工程、AI、创业想法与个人学习。';
-export const SITE_URL = 'https://www.lihuiwang.net';
-export const GITHUB_URL = 'https://github.com/seolhw';
-export const EMAIL = 'seolhw@qq.com';
+export const SITE_TITLE = "huiwang 的博客";
+export const SITE_DESCRIPTION = "记录软件工程、AI、创业想法与个人学习。";
+export const SITE_URL = "https://www.lihuiwang.net";
+export const GITHUB_URL = "https://github.com/seolhw";
+export const EMAIL = "001@huiwang.fun";
